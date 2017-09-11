2102175b9015c4cc724898912d2fb5ff04c96baf
Yashaswini <31819049+Yashaswininittala@users.noreply.github.com>
2017-09-11 13:53:11 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+imprort java.util.*
+public class Main {
+public static void main(String[] args) {
+
+		import java.util.Date;
+public interface StudentArrayOperation {
+void setStudents(Student[] students);
+public class StudentGroup implements StudentArrayOperation {
+public Student[] getStudents() {
+
+		return null;
+	}
+public void setStudents(Student[] students) {
+		public void setStudents( String SName)
+	{
+	Name = SName;
+	}
+	}
+//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+	}
+
+}
